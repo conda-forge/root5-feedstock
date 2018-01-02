fd1338e1615444d84f168b832bed13f9617a4d21
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-01-02 14:54:30 -0800

    Implemented @jakirkham comments

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,31 +2,30 @@
 
 cd build
 
-if [ "$(uname)" == "Darwin" ]; then
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
 
-    cmake .. -Dall=ON -Dkrb5=OFF -Dcocoa=ON -Dgnuinstall=ON -Drpath=ON -Dsoversion=ON -DBUILD_SHARED_LIBS=ON\
+if [ "$(uname)" == "Darwin" ]; then
+    
+    CC=clang
+    CXX=clang++
+    
+    cmake .. -Dall=ON -Dkrb5=ON -Dcocoa=ON -Dgnuinstall=ON -Drpath=ON -Dsoversion=ON -DBUILD_SHARED_LIBS=ON \
+             -Dccache=ON -Dfortran=OFF -Dexplicit_link=ON -Dgsl_shared=ON -Dcling=OFF -Dopengl=ON \
              -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_SYSCONFDIR=${PREFIX}/etc/root \
-             -Dopengl=OFF -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
-             -DCMAKE_FIND_ROOT_PATH=${PREFIX} -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib:${PREFIX}/lib/root" \
-             -DCMAKE_CXX_FLAGS="${CXXFLAGS} -Wl,-rpath,${PREFIX}/lib:${PREFIX}/lib/root" \
-             -DCMAKE_C_FLAGS="${CFLAGS} -Wl,-rpath,${PREFIX}/lib:${PREFIX}/lib/root"
+             -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX}
 
 else
     
     CC=${PREFIX}/bin/gcc
     CXX=${PREFIX}/bin/g++
     
-    cmake .. -Dall=ON -Dkrb5=OFF -Dgnuinstall=ON -Drpath=ON -Dsoversion=ON \
-             -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_SYSCONFDIR=${PREFIX}/etc/root -DCMAKE_FIND_ROOT_PATH=${PREFIX} \
-             -Dopengl=OFF -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} \
-             -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath-link,${PREFIX}/lib:${PREFIX}/lib/root" \
-             -DCMAKE_CXX_FLAGS="-Wl,-rpath-link,${PREFIX}/lib:${PREFIX}/lib/root"
+    cmake .. -Dall=ON -Dkrb5=ON -Dgnuinstall=ON -Drpath=ON -Dsoversion=ON -DBUILD_SHARED_LIBS=ON \
+             -Dccache=ON -Dfortran=OFF -Dexplicit_link=ON -Dgsl_shared=ON -Dcling=OFF -Dopengl=ON  \
+             -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_SYSCONFDIR=${PREFIX}/etc/root \
+             -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX}
 
 fi
-
-# For some reason, CMAKE does not put "-I${PREFIX}/include" in the command line
-# when compiling, with of course disastrous results...
-ln -s $PREFIX/include/* include/
 
 cmake --build . --target install -- -j ${CPU_COUNT}
 
@@ -34,3 +33,8 @@ cmake --build . --target install -- -j ${CPU_COUNT}
 # setting PYTHONPATH
 cp ${PREFIX}/lib/root/libPyROOT.* ${PREFIX}/lib/python2.7/site-packages/
 cp ${PREFIX}/lib/root/ROOT.py ${PREFIX}/lib/python2.7/site-packages/
+
+# Now add the location of the CINT Dynamic Link Library (dll) to the system path
+# for ROOT, so that we do not need to set LD_LIBRARY_PATH at runtime
+sed "s|Unix\.\*\.Root\.DynamicPath\:.*|Unix\.\*\.Root\.DynamicPath\:    \.:${PREFIX}/lib/root/cint/cint/stl:${PREFIX}/lib/root/cint/cint/include|g" ${PREFIX}/etc/root/system.rootrc > new_system.rootrc
+mv new_system.rootrc ${PREFIX}/etc/root/system.rootrc
